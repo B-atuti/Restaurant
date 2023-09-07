ec8c71316a5f05c07c570bbfc1e35c6d8a2da0bc
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 17:07:10 +0300

    complete

--- lib/seed.py
+++ lib/seed.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Restaurant, Customer, Review
 
-def seed_database();
-    engine = create_engine('sqlite:///restaurant.db')
+def seed_database():
+    engine = create_engine('sqlite:///restaurants.db')
     Session = sessionmaker(bind=engine)
-    sessiom = Session
+    session = Session()
 
     fake = Faker()
 
@@ -20,3 +20,27 @@ def seed_database();
         session.add(customer)
         customers.append(customer)    
     
+    restaurants = []
+    for _ in range(10):
+        restaurant = Restaurant(
+            name=fake.company(),
+            price=random.randint(4000, 10000)
+        )
+        session.add(restaurant)
+        restaurants.append(restaurant)
+
+    for restaurant in restaurants:
+        for _ in range(random.randint(1, 10)):
+            customer = random.choice(customers)
+            review = Review(
+                description=fake.sentence(),
+                star_rating=random.randint(1, 10)
+            )
+            restaurant.reviews.append(review)
+            customer.reviews.append(review)
+    
+    session.commit()
+    session.close()
+
+if __name__ == '__main__':
+    seed_database()
