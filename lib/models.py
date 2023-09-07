67b44c1376df3982d1f07afb3e9e0dfe3d58e2d2
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 16:46:20 +0300

    Initial commit

---
+++ lib/models.py
@@ -0,0 +1,18 @@
+# models.py
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_keys=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer)
+
+    reviews = relationship('Review', back_populates='restaurant')
+    customer = relationship('Customer', secondary='reviews')
+    
